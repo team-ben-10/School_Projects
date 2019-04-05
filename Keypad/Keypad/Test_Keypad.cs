e0fcf2bc07f84c0f46b65427893c245ce8eda234
ItzPlayMind <48526348+ItzPlayMind@users.noreply.github.com>
2019-04-05 12:50:46 +0200

    DADADADAD

---
+++ Keypad/Keypad/Test_Keypad.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Keypad : MonoBehaviour
+{
+    TMPro.TextMeshProUGUI text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Keypad.instance.OnButtonClick(Keypad_Click);
+        text = GetComponent<TMPro.TextMeshProUGUI>();
+    }
+
+    public void Keypad_Click(int value)
+    {
+        if (value == -1)
+        {
+            text.text = text.text.Substring(0, text.text.Length - 1);
+        }
+        else
+        {
+            text.text += value + "";
+        }
+    }
+}
