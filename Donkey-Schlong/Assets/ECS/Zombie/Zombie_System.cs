a933bec77674e0cc2577895b9e47bb277762a4f3
ErixCold <erix.cold929gmail.com>
2019-05-23 15:10:28 +0200

    lil edit

---
+++ Donkey-Schlong/Assets/ECS/Zombie/Zombie_System.cs
@@ -0,0 +1,32 @@
+﻿using Unity.Burst;
+using System;
+using System.Collections;
+
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using static Unity.Mathematics.math;
+
+public class Zombie_System : ComponentSystem
+{
+    // This declares a new kind of job, which is a unit of work to do.
+    // The job is declared as an IJobForEach<Translation, Rotation>,
+    // meaning it will process all entities in the world that have both
+    // Translation and Rotation components. Change it to process the component
+    // types you want.
+    //
+    // The job is also tagged with the BurstCompile attribute, which means
+    // that the Burst compiler will optimize it for the best performance.
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref Zombie_Comp zomb) =>
+        {
+            zomb.speed += 1f * Time.deltaTime;
+            Debug.Log(zomb.speed);
+        });
+    }
+}
---
+++ Donkey-Schlong/Assets/Normal Scripts/Testing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class Testing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        EntityManager entMan = World.Active.EntityManager;
+        var ent =entMan.CreateEntity(typeof(Zombie_Comp));
+
+        entMan.SetComponentData(ent, new Zombie_Comp { speed = 10, jumpHeight = 5 });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
